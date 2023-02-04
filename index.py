658b51372871c78e6fe29c8b2b8789e1c883c21c
Nandang <77793855+BegalNenen@users.noreply.github.com>
2023-02-04 16:28:49 +0700

    Update index.py

--- index.py
+++ index.py
@@ -5,37 +5,50 @@ app = Flask(__name__, template_folder="html", static_folder="assets")
 app.url_map.strict_slashes = False
 
 @app.errorhandler(404)
-def page_not_found(error):
+def page_not_found(e):
     return render_template('404.html'), 404
-
+@app.route('/about')
+def about():
+    return render_template('about.html')    
+            
+# AL-QURAN
+###############
 @app.route('/')
 def ayat():
     url = 'https://equran.id/api/v2/surat/'
     response = requests.get(url)
     data = response.json()
-    return render_template('index.html', data=data)
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
+    return render_template('alquran/index.html', data=data)
     
-# buat bikin page custom untuk ayat
-pages = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '70', '71', '72', '73', '74', '75', '76', '77', '78', '79', '80', '81', '82', '83', '84', '85', '86', '87', '88', '89', '90', '91', '92', '93', '94', '95', '96', '97', '98', '99', '100', '101', '102', '103', '104', '105', '106', '107', '108', '109', '110', '111', '112', '113', '114']
-@app.route('/surat/<page>')
-def surat(page):
-    if page not in pages:
-        return render_template('404.html'), 404
-    url = f'https://equran.id/api/v2/surat/{page}'
+# For Loop untuk membuat halaman dengan nama angka secara otomatis
+numbers = [i for i in range(1, 114)]
+@app.route('/ayat/<int:n>')
+def isi(n):
+    url = f'https://equran.id/api/v2/surat/{n}'
+    response = requests.get(url)
+    if response.status_code == 404:
+        return render_template('404.html', error="Halaman tidak ditemukan")
+    data = response.json()
+    return render_template('alquran/ayat.html', data=data)
+
+# DOA
+##############
+@app.route('/doa')
+def Doa():
+    url = 'https://equran.id/api/doa/'
     response = requests.get(url)
     data = response.json()
-    return render_template('ayat.html', data=data)
-
-@app.route('/surat/<path:filename>')
-def custom_static(filename):
-    try:
-        return send_from_directory(app.static_folder, filename)
-    except FileNotFoundError:
-        abort(404)
+    return render_template('/doa/index.html', data=data)
+    
+numbers = [i for i in range(1, 114)]
+@app.route('/doa/<int:n>')
+def Doanya(n):
+    url = f'https://equran.id/api/doa/{n}'
+    response = requests.get(url)
+    if response.status_code == 404:
+        return render_template('404.html', error="Halaman tidak ditemukan")
+    data = response.json()
+    return render_template('doa/doa.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
